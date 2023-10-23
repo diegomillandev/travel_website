a91cf6b759ff4db0e28ba3d0ed3d2d7ee6c27871
Diego Millan <diegomillandev@gmail.com>
2023-10-22 19:34:14 -0500

    Create Section Camp

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
-  title: "Travel",
+  title: "Travel App",
   description: "Travel UI/UX App for Camping",
 };
 
